2fd43d1f2c9bf873945bf8cbec71def28cfa479e
Eldor Togaymurotov <eldortogaymurotov@Eldors-MacBook-Pro.local>
2022-10-27 12:58:58 -0400

    aws upload

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "expoimageupload32b7d6a3": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "s3e9fd23a6": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
